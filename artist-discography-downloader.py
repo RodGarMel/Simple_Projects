4980f86efde6a0550d22ad4778b59368cdfb60d9
RodGarMel <rodrigomelendez.rgm@gmail.com>
2025-08-10 15:06:48 -0600

    Fixed the function for searching releases and now the code is able to search for songs in an album too

--- artist-discography-downloader.py
+++ artist-discography-downloader.py
@@ -19,23 +19,39 @@ def search_artist(artist_name):
     return data.get("artists", [])
 
 def search_releases(artist_id, album_name, limit=10):
-    time.sleep(2)
-    print(f"INICIANDO BUSQUEDA DE RELEASES PARA EL ALBUM {album_name}")
+    time.sleep(1)
     url= "https://musicbrainz.org/ws/2/release/"
     params ={
-        "artist": artist_id,
-        "release": album_name,
+        "query": f'arid:{artist_id} AND release:"{album_name}"', #Forzamos la búsqueda a través de un query para encontrar coincidencias exactas
         "fmt": "json",
         "limit": limit
     }
-    print(f"PARAMS ENVIADOS: {params}")
     response = requests.get(url, params=params, headers=HEADERS)
     response.raise_for_status()  # Lanza error si la respuesta no es 200
     data = response.json()
     return data.get("releases", [])
 
+def search_songs(release_id):
+    time.sleep(1)
+    url= f"https://musicbrainz.org/ws/2/release/{release_id}" #URL must contain the ID of the album
+    params ={
+        "inc": "recordings",
+        "fmt": "json",
+    }
+    response = requests.get(url, params=params, headers=HEADERS)
+    response.raise_for_status()  # Lanza error si la respuesta no es 200
+    data = response.json()
+    tracks = []
+    for medium in data.get("media", []):
+        for track in medium.get("tracks", []):
+            track_title = track.get("title")
+            if track_title:
+                tracks.append(track_title)
+    
+    return tracks
+
 def search_albums(artist_id, limit=100):
-    time.sleep(2)
+    time.sleep(1)
     url= "https://musicbrainz.org/ws/2/release-group/"
     params ={
         "artist": artist_id,
@@ -63,7 +79,8 @@ for artist in results:
     lifespan = artist.get('life-span', {})
     begin = lifespan.get('begin', '???')
     artist_id = artist.get('id')
-    print(f"- {artist['name']} (MBID: {artist['id']}) Score: {artist['score']} Life-span: {begin} Type: {artist['type']}")
+    # print(f"- {artist['name']} (MBID: {artist['id']}) Score: {artist['score']} Life-span: {begin} Type: {artist['type']}")
+    print(f"-Name: {artist['name']} // Life-span: {begin} // Type: {artist['type']}")
 
     albums = search_albums(artist_id)
 
@@ -92,28 +109,42 @@ for artist in results:
         if i[1] in ["Album", "EP"]: # Filter only Albums and EPs
             album_name_list.append(i[0]) #Add the name of the Album/EP to a list
 
-    print(album_name_list) #Debugging
+    print(f"Algunos álbumes del artista: {album_name_list}") #Debugging
 
-    #-----------------------------ARREGLAR----------------------------- 
     release_ids = []
     
     for album_name in album_name_list:
-        print(f"Item actual: {album_name}")
-        print(f"Llamando función buscar para item: {album_name}")
         releases = search_releases(artist_id, album_name)
-        print(releases)
-        print("///////////////////////////////")
-    
-        # releases = search_releases(artist_id, album_name)
-        
-        # print(releases)
-    
-    #-----------------------------ARREGLAR----------------------------- 
 
-    if album_song_list:
-        print(album_song_list)
-    else:
-        print("No se encontraron canciones para el álbum")
+        release_id = None
+        if releases:
+            for release in releases:
+                primary_type = release.get("release-group", {}).get("primary-type", "")
+                # track_count = str_to_int(release.get("track-count", ""))
+                track_count = release.get("track-count", "")
+                if (primary_type in ["Album","EP"]) and track_count <=30:
+                    release_id = release.get('id')
+                    release_ids.append([album_name, release_id])
+                    # print(f"ID encontrado para {album_name}: {release_id}") #Debugging 
+                    break
+        else:
+            print(f"No se encontró un release_id para: {album_name}")
+    
+    # print(release_ids) #Debugging
+
+    for id in release_ids:
+        songs = search_songs(id[1])
+        album_song_list.append([id[0], songs])
+
+    print(album_song_list) #Debugging 
+        
+
+
+
+    # if album_song_list:
+    #     print(album_song_list)
+    # else:
+    #     print("No se encontraron canciones para el álbum")
 
     answer = input("Is this your artist? Type YES or NO \n")
 
